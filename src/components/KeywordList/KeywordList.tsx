6ba473c1689b4d143849a973f05dc1d99ca0c003
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-05-25 17:33:20 -0400

    Update KeywordList header

--- src/components/KeywordList/KeywordList.tsx
+++ src/components/KeywordList/KeywordList.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react'
+import Header from '@components/Header'
+import { useCallback, useMemo } from 'react'
 import KeywordItem from './KeywordItem'
 
 export default function KeywordList({
@@ -9,17 +10,35 @@ export default function KeywordList({
 	resume: string
 }) {
 	const formattedResume = useMemo(() => resume.toLowerCase(), [resume])
+	const keywordMatched = useCallback(
+		(keyword: string) => formattedResume.includes(keyword.toLowerCase()),
+		[formattedResume],
+	)
+
+	const matchCount = useMemo(
+		() =>
+			keywords.reduce(
+				(acc, keyword) => (keywordMatched(keyword) ? acc + 1 : acc),
+				0,
+			),
+		[formattedResume],
+	)
 
 	return (
-		<div className="flex flex-col gap-2">
-			{keywords.map((keyword) => (
-				<span key={keyword}>
-					<KeywordItem
-						isMatched={formattedResume.includes(keyword.toLowerCase())}
-						keyword={keyword}
-					/>
-				</span>
-			))}
+		<div className="flex flex-col gap-4">
+			<Header
+				value={`Keyword - ${matchCount}/${keywords.length} (${Math.floor((matchCount / keywords.length) * 100)}%)`}
+			/>
+			<div className="flex flex-col gap-2">
+				{keywords.map((keyword) => (
+					<span key={keyword}>
+						<KeywordItem
+							isMatched={keywordMatched(keyword)}
+							keyword={keyword}
+						/>
+					</span>
+				))}
+			</div>
 		</div>
 	)
 }
