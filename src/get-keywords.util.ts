34afdbeda69d0a308d7da97ea3e6d2689da6ab33
Thiv Rajan <thiv.nadarajan@gmail.com>
2024-05-30 17:16:23 -0400

    Add error handling to keyword generation (#6)
    * Add error handling
    
    * Update ErrorMessage display time

--- src/get-keywords.util.ts
+++ src/get-keywords.util.ts
@@ -1,18 +1,25 @@
+import { showErrorMessage } from './error-message.util'
+
 export async function getKeywords(jobDescription: string) {
-	const response = await fetch(import.meta.env.VITE_KEYWORDS_ENDPOINT, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			jobDescription,
-		}),
-	})
-	const responseJSON = await response.json()
+	try {
+		const response = await fetch(import.meta.env.VITE_KEYWORDS_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				jobDescription,
+			}),
+		})
+		const responseJSON = await response.json()
 
-	if (responseJSON.error) {
-		throw Error(responseJSON.error)
+		if (responseJSON.error || !responseJSON.keywords?.length) {
+			throw Error()
+		}
+
+		return responseJSON.keywords as string[]
+	} catch (error) {
+		showErrorMessage('Unable generate keywords')
+		return []
 	}
-
-	return responseJSON.keywords as string[]
 }
